eb96b9cacc0a996082bee308a803858980cd881e
Mario <mario.perez094@gmail.com>
2024-04-06 09:42:03 -0700

    landing_page started

---
+++ app/javascript/src/pages/landingPage/landingPage.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function LandingPage() {
+  return (
+    <div>Hi</div>
+  )
+};
+
+export default LandingPage;
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,18 @@
 const { environment } = require('@rails/webpacker')
 
+const path = require('path')
+
+const customConfig = {
+  resolve: {
+    alias: {
+      '@src': path.resolve(__dirname, '..', '..', 'app/javascript/src'),
+      '@packs': path.resolve(__dirname, '..', '..', 'app/javascript/packs'),
+      '@pages': path.resolve(__dirname, '..', '..', 'app/javascript/src/pages'),
+      '@components': path.resolve(__dirname, '..', '..', 'app/javascript/components'),
+    }
+  }
+}
+
+environment.config.merge(customConfig);
+
 module.exports = environment
